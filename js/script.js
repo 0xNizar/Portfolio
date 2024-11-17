0c36c4254d13279de8102bc5d069c3af8771fb7d
0xNizar <gainonce01@gmail.com>
2024-11-17 06:18:53 +0300

    Simple website

---
+++ js/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll("a[href^='#']");
+    links.forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute("href")).scrollIntoView({
+                behavior: "smooth"
+            });
+        });
+    });
+
+    ScrollReveal().reveal('.landing-text, .avatar, .contact-section', { delay: 500, distance: '100px', origin: 'bottom', duration: 1500 });
+
+    const backToTopBtn = document.getElementById("back-to-top");
+
+    window.onscroll = function () {
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+            backToTopBtn.style.display = "block";
+        } else {
+            backToTopBtn.style.display = "none";
+        }
+    };
+    
+    window.scrollToTop = function () {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+});
